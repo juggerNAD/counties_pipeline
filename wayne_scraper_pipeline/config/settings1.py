0702b102cf2526151c0772bd7c8f23043046498d
juggerNAD <143319093+juggerNAD@users.noreply.github.com>
2026-02-07 16:44:54 +0800

    Create settings1.py

---
+++ wayne_scraper_pipeline/config/settings1.py
@@ -0,0 +1,18 @@
+# URLs and settings
+PROBATE_URL = "https://public.wcpc.us/eservices/searchresults.page?x=6YiTrMq36Q2DAH62GlxoQHBZO0cVrs1P2cgQo*IE*inRWgJcfccziGznRUT--OBrRB9FzlEqOgfg6ABDu9*zKA"
+AUDITOR_URL = "https://bsaonline.com/SiteSearch/SiteSearchResults"
+
+CASE_TYPES = ["DE Dec Estate Non Supervised", "DA Dec Estate Supervised"]
+
+# Date range filter example
+DATE_FROM = "01/01/2025"
+DATE_TO = "12/31/2025"
+
+# Download folder
+PDF_DOWNLOAD_PATH = "output/pdfs/"
+
+# Selenium options
+HEADLESS = False  # Set True to run without opening browser
+
+# Logging
+LOG_FILE = "logs/scraper.log"

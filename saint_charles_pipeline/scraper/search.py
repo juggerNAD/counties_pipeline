9b88f1ed1d7e49fa8f9368e33fa45674b9baf7c8
juggerNAD <143319093+juggerNAD@users.noreply.github.com>
2026-02-08 15:23:57 +0800

    Update search.py

--- saint_charles_pipeline/scraper/search.py
+++ saint_charles_pipeline/scraper/search.py
@@ -1,1 +1,15 @@
+from datetime import date, timedelta
+
+def build_search_payload(config):
+    end = date.today()
+    start = end - timedelta(days=config["days_window"])
+
+    return {
+        "countyCode": config["county_code"],
+        "courtCode": config["court_code"],
+        "caseType": config["case_type"],
+        "startDate": start.strftime("%m/%d/%Y"),
+        "endDate": end.strftime("%m/%d/%Y"),
+        "newSearch": "Y"
+    }
 

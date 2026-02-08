b577061aa506f1a07b63790aee6c8b09a2fead05
juggerNAD <143319093+juggerNAD@users.noreply.github.com>
2026-02-08 15:22:53 +0800

    Update pdf_downloader.py

--- saint_charles_pipeline/scraper/pdf_downloader.py
+++ saint_charles_pipeline/scraper/pdf_downloader.py
@@ -1,1 +1,13 @@
+import os
+
+def download_pdf(session, url, save_dir):
+    os.makedirs(save_dir, exist_ok=True)
+    filename = url.split("=")[-1] + ".pdf"
+    path = os.path.join(save_dir, filename)
+
+    r = session.get(url)
+    with open(path, "wb") as f:
+        f.write(r.content)
+
+    return path
 

8af6d296b3d1d88db2ce3c4531facdb7dc034fda
juggerNAD <143319093+juggerNAD@users.noreply.github.com>
2026-02-08 15:22:36 +0800

    Update normalizers.py

--- saint_charles_pipeline/scraper/normalizers.py
+++ saint_charles_pipeline/scraper/normalizers.py
@@ -1,1 +1,8 @@
+import re
+
+def normalize_phone(phone):
+    digits = re.sub(r"\D", "", phone)
+    if len(digits) == 10:
+        return f"{digits[:3]}-{digits[3:6]}-{digits[6:]}"
+    return None
 

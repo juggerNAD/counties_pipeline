86402ef97c8fe0e33951e3a95c7a27960ad0b07a
juggerNAD <143319093+juggerNAD@users.noreply.github.com>
2026-02-08 15:24:15 +0800

    Update session.py

--- saint_charles_pipeline/scraper/session.py
+++ saint_charles_pipeline/scraper/session.py
@@ -1,1 +1,15 @@
+import requests
+from configparser import ConfigParser
+
+class CourtSession:
+    def __init__(self, headers, timeout):
+        self.session = requests.Session()
+        self.session.headers.update(headers)
+        self.timeout = timeout
+
+    def get(self, url, params=None):
+        return self.session.get(url, params=params, timeout=self.timeout)
+
+    def post(self, url, data):
+        return self.session.post(url, data=data, timeout=self.timeout)
 

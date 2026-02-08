ce0a78384ad7d25c91ea111ac2be5d53c3eac2a2
juggerNAD <143319093+juggerNAD@users.noreply.github.com>
2026-02-08 15:22:14 +0800

    Update exceptions.py

--- saint_charles_pipeline/scraper/exceptions.py
+++ saint_charles_pipeline/scraper/exceptions.py
@@ -1,1 +1,43 @@
+"""
+Custom exceptions for Missouri Case.net Probate Scraper
+"""
+
+class ScraperError(Exception):
+    """Base scraper exception"""
+    pass
+
+
+class SessionInitError(ScraperError):
+    """Raised when HTTP session cannot be initialized"""
+    pass
+
+
+class SearchError(ScraperError):
+    """Raised when case search fails"""
+    pass
+
+
+class CaseParseError(ScraperError):
+    """Raised when case list parsing fails"""
+    pass
+
+
+class CaseDetailError(ScraperError):
+    """Raised when case detail page fails to load"""
+    pass
+
+
+class DocketParseError(ScraperError):
+    """Raised when docket entries cannot be parsed"""
+    pass
+
+
+class PDFDownloadError(ScraperError):
+    """Raised when PDF download fails"""
+    pass
+
+
+class PDFParseError(ScraperError):
+    """Raised when PDF text extraction fails"""
+    pass
 
